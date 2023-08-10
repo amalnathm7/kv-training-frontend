3a6143eb3c41015eb7910f9c8dbea56dc1a33c46
Amal Nath M <amalnathm7@gmail.com>
2023-08-10 21:03:04 +0530

    Sub header added

--- src/layouts/home-layout/HomeLayout.tsx
+++ src/layouts/home-layout/HomeLayout.tsx
@@ -1,12 +1,20 @@
 import Sidebar from "../../components/sidebar/Sidebar";
 import Header from "../../components/header/Header";
+import SubHeader from "../../components/sub-header/SubHeader";
 import React from "react";
 import "./HomeLayout.css";
 
-const HomeLayout: React.FC = () => {
+type HomeLayoutPropsType = {
+    subHeaderLabel: string,
+    subHeaderActionLabel: string,
+    subHeaderActionIcon: string,
+}
+
+const HomeLayout: React.FC<HomeLayoutPropsType> = (props) => {
     return <div className="home">
-        <Sidebar/>
-        <Header/>
+        <Sidebar />
+        <Header />
+        <SubHeader label={props.subHeaderLabel} actionLabel={props.subHeaderActionLabel} actionIcon={props.subHeaderActionIcon} />
     </div>;
 };
 
