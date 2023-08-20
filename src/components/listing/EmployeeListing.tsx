46ff7c558607db7fce3032f13ac13165735a5cca
Amal Nath M <amalnathm7@gmail.com>
2023-08-21 00:27:52 +0530

    useState and useEffect added for EmployeeListing.tsx

--- src/components/listing/EmployeeListing.tsx
+++ src/components/listing/EmployeeListing.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Listing.css";
 import EmployeeListItem from "../list-item/EmployeeListItem";
 import { useGetEmployeeListQuery } from "../../services/employeeApi";
@@ -8,14 +8,16 @@ type EmployeeListingPropsType = {
 }
 
 const EmployeeListing: React.FC<EmployeeListingPropsType> = (props) => {
-    const { data: employeesData, isSuccess } = useGetEmployeeListQuery();
+    const { data: employeesData, isSuccess: isEmployeesFetchSuccess } = useGetEmployeeListQuery();
 
     const labels = props.labels.map((label) => <td className="listing-label" key={label}><label>{label}</label></td>);
 
-    let employees = [];
+    const [employees, setEmployees] = useState([]);
 
-    if (isSuccess)
-        employees = employeesData.data.map((employee) => <EmployeeListItem key={employee.id} employee={employee}></EmployeeListItem>);
+    useEffect(() => {
+        if (isEmployeesFetchSuccess)
+            setEmployees(employeesData.data.map((employee) => <EmployeeListItem key={employee.id} employee={employee}></EmployeeListItem>));
+    }, [isEmployeesFetchSuccess]);
 
     return <div className="listing">
         <table>
