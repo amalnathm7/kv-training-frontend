6e59ebcf9e3e21de86963bbdb806c3296ea4d359
Amal Nath M <amalnathm7@gmail.com>
2023-08-21 17:27:25 +0530

    Nav bar modified

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import './Sidebar.css';
 import SideBarButton from '../button/SideBarButton/SideBarButton';
+import { RouteConstants } from '../../constants/routeConstants';
 
 const Sidebar: React.FC = () => {
   return (
     <div className='sidebar'>
-      <SideBarButton imgIcon='/assets/icons/employees.svg' headerText='Employee List' />
-      <SideBarButton imgIcon='/assets/icons/employees.svg' headerText='All Referrals' />
-      <SideBarButton imgIcon='/assets/icons/employees.svg' headerText='Job Openings' />
-      <SideBarButton imgIcon='/assets/icons/employees.svg' headerText='My Referrals' />
+      <div className='sidebar-items'>
+        <SideBarButton
+          route={RouteConstants.employee}
+          imgIcon='/assets/icons/employees.svg'
+          headerText='Employee List'
+        />
+        <SideBarButton
+          route={RouteConstants.opening}
+          imgIcon='/assets/icons/job_icon.png'
+          headerText='Job Openings'
+        />
+        <SideBarButton
+          route={RouteConstants.myReferral}
+          imgIcon='/assets/icons/my_referrals.png'
+          headerText='My Referrals'
+        />
+        <SideBarButton
+          route={RouteConstants.referral}
+          imgIcon='/assets/icons/all_referrals.png'
+          headerText='All Referrals'
+        />
+      </div>
     </div>
   );
 };
