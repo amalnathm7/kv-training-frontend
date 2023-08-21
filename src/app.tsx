e2d40d5cf9fa382ab2a0cbbf58e59dd6bd78f206
Devi Krishna M K <devikrishnamk0123@gmail.com>
2023-08-21 15:52:09 +0530

    changes done

--- src/app.tsx
+++ src/app.tsx
@@ -18,27 +18,25 @@ import ReferralDetailsPage from './pages/referral/ReferralDetailsPage';
 
 const App: FC = () => {
   return (
-    (
     <div className='app'>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<SplashPage />} />
-            <Route path={RouteConstants.login} element={<LoginPage />} />
-            <Route path={RouteConstants.employee} element={<EmployeeListingPage />} />
-            <Route path={`${RouteConstants.employee}/:id`} element={<EmployeeDetailsPage />} />
-            <Route path={`${RouteConstants.employee}/create`} element={<EmployeeCreatePage />} />
-            <Route path={`${RouteConstants.employee}/:id/edit`} element={<EmployeeEditPage />} />
-          <Route path={`${RouteConstants.referralsApi}`} element={<ReferralListingPage />} />
-            <Route path={`${RouteConstants.opening}`} element={<OpeningListingPage />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SplashPage />} />
+          <Route path={RouteConstants.login} element={<LoginPage />} />
+          <Route path={RouteConstants.employee} element={<EmployeeListingPage />} />
+          <Route path={`${RouteConstants.employee}/:id`} element={<EmployeeDetailsPage />} />
+          <Route path={`${RouteConstants.employee}/create`} element={<EmployeeCreatePage />} />
+          <Route path={`${RouteConstants.employee}/:id/edit`} element={<EmployeeEditPage />} />
+          <Route path={`${RouteConstants.referralApi}`} element={<ReferralListingPage />} />
+          <Route path={`${RouteConstants.opening}`} element={<OpeningListingPage />} />
           <Route path={`${RouteConstants.opening}/:id`} element={<OpeningDetailsPage />} />
           <Route path={`${RouteConstants.opening}/:id/refer`} element={<ReferralCreatePage />} />
           <Route path={`${RouteConstants.opening}/create`} element={<OpeningCreatePage />} />
           <Route path={`${RouteConstants.opening}/:id/edit`} element={<OpeningEditPage />} />
           <Route path={`${RouteConstants.referralApi}/:id`} element={<ReferralDetailsPage />} />
         </Routes>
-        </BrowserRouter>
-      </div>
-  )
+      </BrowserRouter>
+    </div>
   );
 };
 
