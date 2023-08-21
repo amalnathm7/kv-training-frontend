05d6542402fe2bfb84896beb54a57e5a6f3df1cc
Bharath Chandran <bharathvellappillil@gmail.com>
2023-08-21 16:32:30 +0530

    conflicts resolved

--- src/services/referralApi.ts
+++ src/services/referralApi.ts
@@ -43,5 +43,6 @@ export const {
   useCreateReferralMutation,
   useUpdateReferralMutation,
   useGetReferralByIdQuery,
-  useGetMyReferralsQuery
+  useGetMyReferralsQuery,
+  useGetAllReferralsListQuery
 } = referralApi;
