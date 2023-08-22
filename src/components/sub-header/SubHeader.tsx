42cd2d9434e4827c3f30e1ffb4acbb09f3aec50f
Amal Nath M <amalnathm7@gmail.com>
2023-08-22 10:39:24 +0530

    Classname added for div

--- src/components/sub-header/SubHeader.tsx
+++ src/components/sub-header/SubHeader.tsx
@@ -17,28 +17,30 @@ const SubHeader: React.FC<SubHeaderPropsType> = (props) => {
     <div className='sub-header'>
       <label className='sub-header-label'>{props.label}</label>
       {/* {props.searchClicked && <input type='text' placeholder='Search by email / referral Id' onChange={handleSearch}/>} */}
-      {props.primaryActionLabel.length > 0 && props.primaryActionLabel != 'Search' && (
-        <div className='action-button' onClick={props.primaryAction}>
-          <div className='sub-header-action-icon-container'>
-            <img
-              className='sub-header-action-icon'
-              src={'/assets/icons/' + props.primaryActionIcon}
-            ></img>
+      <div className='sub-header-actions'>
+        {props.primaryActionLabel.length > 0 && props.primaryActionLabel != 'Search' && (
+          <div className='action-button' onClick={props.primaryAction}>
+            <div className='sub-header-action-icon-container'>
+              <img
+                className='sub-header-action-icon'
+                src={'/assets/icons/' + props.primaryActionIcon}
+              ></img>
+            </div>
+            <label className='sub-header-action-label'>{props.primaryActionLabel}</label>
           </div>
-          <label className='sub-header-action-label'>{props.primaryActionLabel}</label>
-        </div>
-      )}
-      {props.secondaryActionLabel?.length > 0 && (
-        <div className='action-button' onClick={props.secondaryAction}>
-          <div className='sub-header-action-icon-container'>
-            <img
-              className='sub-header-action-icon'
-              src={'/assets/icons/' + props.secondaryActionIcon}
-            ></img>
+        )}
+        {props.secondaryActionLabel?.length > 0 && (
+          <div className='action-button' onClick={props.secondaryAction}>
+            <div className='sub-header-action-icon-container'>
+              <img
+                className='sub-header-action-icon'
+                src={'/assets/icons/' + props.secondaryActionIcon}
+              ></img>
+            </div>
+            <label className='sub-header-action-label'>{props.secondaryActionLabel}</label>
           </div>
-          <label className='sub-header-action-label'>{props.secondaryActionLabel}</label>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
