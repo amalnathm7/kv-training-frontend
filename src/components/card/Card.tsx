d4f316af55add5549d6a57afecd05916c4f2c604
Bharath Chandran <bharathvellappillil@gmail.com>
2023-08-22 18:30:42 +0530

    removed from details page

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CardItem from '../card-item/CardItem';
 import './Card.css';
 import SecondaryButton, {
   SecondaryButtonPropsType
 } from '../button/SecondaryButton/SecondaryButton';
+import { useGetMyProfileQuery } from '../../services/employeeApi';
+import { PermissionLevel } from '../../utils/PermissionLevel';
 
 type CardItemType = {
   label: string;
@@ -17,14 +19,33 @@ export type CardPropsType = {
 };
 
 const Card: React.FC<CardPropsType> = (props) => {
-  const items = props.items.map((item: CardItemType) => (
-    <CardItem
-      key={item.value}
-      label={item.label}
-      value={item.value}
-      isStatus={item.isStatus ? item.isStatus : false}
-    />
-  ));
+  const { data: myProfile, isSuccess: isMyProfileFetchSuccess } = useGetMyProfileQuery();
+  const [isSuperAuthorized, setIsSuperAuthorized] = useState(false);
+
+  useEffect(() => {
+    if (isMyProfileFetchSuccess && myProfile.data.role?.permissionLevel === PermissionLevel.SUPER)
+      setIsSuperAuthorized(true);
+  }, [isMyProfileFetchSuccess]);
+
+  const items = props.items.map((item: CardItemType) =>
+    ['Employee ID', 'Opening ID', 'Referral ID'].includes(item.label) ? (
+      isSuperAuthorized && (
+        <CardItem
+          key={item.value}
+          label={item.label}
+          value={item.value}
+          isStatus={item.isStatus ? item.isStatus : false}
+        />
+      )
+    ) : (
+      <CardItem
+        key={item.value}
+        label={item.label}
+        value={item.value}
+        isStatus={item.isStatus ? item.isStatus : false}
+      />
+    )
+  );
 
   return (
     <div>
